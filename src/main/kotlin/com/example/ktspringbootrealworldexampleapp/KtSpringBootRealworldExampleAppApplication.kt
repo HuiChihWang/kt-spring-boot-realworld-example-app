b13f17122747104c913f118d077eca0d8d831878
Gilbert Wang (Payment Team) <gilbert.wang@kkday.com>
2024-07-01 23:35:10 +0800

    init project

---
+++ src/main/kotlin/com/example/ktspringbootrealworldexampleapp/KtSpringBootRealworldExampleAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktspringbootrealworldexampleapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtSpringBootRealworldExampleAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtSpringBootRealworldExampleAppApplication>(*args)
+}
